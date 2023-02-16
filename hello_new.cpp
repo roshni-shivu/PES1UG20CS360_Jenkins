9058b986537a81170c767bd25a1f43aa8136baa9
roshni-shivu <80108378+roshni-shivu@users.noreply.github.com>
2023-02-16 23:52:18 +0530

    Add files via upload

---
+++ hello_new.cpp
@@ -0,0 +1,11 @@
+//File: hello_new.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I am Roshni Shivu,PES1UG20CS360\n";
+  cout << "Hi! It's me, I am th problem it's me\n";
+  return 0;
+}
