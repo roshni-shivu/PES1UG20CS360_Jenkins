3d997087c2c88d8b602e0d157321fad11cd5c68e
roshni-shivu <80108378+roshni-shivu@users.noreply.github.com>
2023-02-16 23:38:41 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "This is Roshni Shivu , PES1UG20CS360 \n";
-  cout << "I have successfully built and run\n";
+  cout << "Life is tough :( \n";
   return 0;
 }
